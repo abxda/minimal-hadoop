c69d6f2af6919e8ada39ebdbfffe27ef25142b83
abxda <abxda@outlook.com>
2022-11-25 15:13:56 -0600

    configure updates

--- configure-hadoop.sh
+++ configure-hadoop.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
  
+ echo  "if [ -f /vagrant/hadoop-env-user.sh ]; then"  >> ~/.bashrc
+ echo  "  . /vagrant/hadoop-env-user.sh"  >> ~/.bashrc 
+ echo  "fi" >> ~/.bashrc
+ . /home/vagrant/.bashrc
+ 
+ . /vagrant/hadoop-env-user.sh
+
  ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
  cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
  chmod 600 ~/.ssh/authorized_keys
- echo -e "\nif [ -f /vagrant/hadoop-env-user.sh ]; then\n    . /vagrant/hadoop-env-user.sh\nfi\n" >> ~/.bashrc
+ 
  mkdir -p /home/vagrant/hdfs/namenode
  mkdir -p /home/vagrant/hdfs/datanode
- cd /home/vagrant
- sh .bashrc
+ 
  hadoop namenode -format
-
 
